058bfccca9b9b72cd4c052e656007b26bfc26608
habib4224 <4224habib1@gmail.com>
2022-11-29 20:48:43 +0600

    commit

--- src/Pages/EachCatagories/EachCatagories.js
+++ src/Pages/EachCatagories/EachCatagories.js
@@ -9,8 +9,6 @@ const EachCatagories = () => {
     console.log("Eta brand name ", data);
     return (
         <div>
-            {data.length}
-
             <div className='grid gap-6 grid-cols-1 md:grid-cols-2 lg:grid-cols-3'>
                 {
                     data.map(subCategory =>

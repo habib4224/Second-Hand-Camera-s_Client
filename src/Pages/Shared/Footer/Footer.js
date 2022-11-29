c27458ccc0ce4cb21aeea011f48318e164d2f105
habib4224 <4224habib1@gmail.com>
2022-11-29 21:37:54 +0600

    commit

--- src/Pages/Shared/Footer/Footer.js
+++ src/Pages/Shared/Footer/Footer.js
@@ -5,9 +5,6 @@ const Footer = () => {
     return (
         <footer className="footer footer-center p-10 bg-base-200 text-base-content rounded">
             <div className="grid grid-flow-col gap-4">
-                <Link className="link link-hover">About us</Link>
-                <Link className="link link-hover">Contact</Link>
-
             </div>
             <div>
                 <div className="grid grid-flow-col gap-4">

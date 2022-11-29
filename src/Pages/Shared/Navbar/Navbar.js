ef22b1221e53e6ea09cb7f6b12bd64bc2f147fde
habib4224 <4224habib1@gmail.com>
2022-11-29 21:45:45 +0600

    commit

--- src/Pages/Shared/Navbar/Navbar.js
+++ src/Pages/Shared/Navbar/Navbar.js
@@ -20,7 +20,7 @@ const Navbar = () => {
             .then(() => { })
             .catch(error => console.log(error));
     }
-    const menuItems = <React.Fragment>
+    const navberItem = <React.Fragment>
         <li><Link to='/'>Home</Link></li>
         <li><Link to='/blogs'>Blogs</Link></li>
         {user?.uid ?
@@ -39,14 +39,14 @@ const Navbar = () => {
                         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                     </label>
                     <ul tabIndex={1} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
-                        {menuItems}
+                        {navberItem}
                     </ul>
                 </div>
-                <Link to='/' className="btn btn-ghost normal-case text-xl">Value Your Product</Link>
+                <Link to='/' className="btn hidden lg:flex btn-ghost normal-case text-xl">Value Your Product</Link>
             </div>
-            <div className="navbar-center hidden lg:flex">
+            <div className="navbar-end hidden lg:flex">
                 <ul className="menu menu-horizontal p-0">
-                    {menuItems}
+                    {navberItem}
                 </ul>
             </div>
             <label htmlFor='dashboard-drawer' tabIndex={2} className="btn btn-ghost lg:hidden">
